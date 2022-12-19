7b922076cc198b6674299e42104e4497c290a98a
VictorLima <victoraugustolima1@gmail.com>
2022-12-19 20:57:15 -0300

    crud parcialmente completo

--- AluraGeekMVCTeste/Controllers/ProdutosController.cs
+++ AluraGeekMVCTeste/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
-﻿using AluraGeekMVCTeste.Models.Services;
+﻿using AluraGeekMVCTeste.Models;
+using AluraGeekMVCTeste.Models.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AluraGeekMVCTeste.Controllers
@@ -22,9 +23,81 @@ namespace AluraGeekMVCTeste.Controllers
             //retornar view
             return View(produtos);
         }
+        //metodo get assincrono, obter produto
+        public async Task<IActionResult> Details(int id)
+        {
+            //obter produto
+            var produto = await _produtosServices.GetProdutoAsync(id);
+            //retornar view
+            return View(produto);
+        }
+
+        //metodo get assincrono, criar um produto
+        public IActionResult Create()
+        {
+            return View();
+        }
 
 
 
+        //metodo post assincrono, criar um produto
+        [HttpPost]
+        public async Task<IActionResult> Create(Produtos produto)
+        {
+            //verificar se o modelo é valido
+            
+                //criar produto
+                await _produtosServices.CreateProdutoAsync(produto);
+            //redirecionar para a pagina inicial
+
+            return RedirectToAction(nameof(Index));
+           
+        }
+
+        //metodo get assincrono, editar um produto
+        public async Task<IActionResult> Edit(int id)
+        {
+            //obter produto
+            var produto = await _produtosServices.GetProdutoAsync(id);
+            //retornar view
+            return View(produto);
+        }
+
+
+
+        //metodo put assincrono, edita um produto
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Produtos produto)
+        {
+            //verificar se o modelo é valido
+            if (ModelState.IsValid)
+            {
+                //editar produto
+                await _produtosServices.UpdateProdutoAsync(produto, id);
+                //redirecionar para a pagina inicial
+                return RedirectToAction(nameof(Index));
+            }
+            //retornar view
+            return View(produto);
+        }
+        //metodo get assincrono, deletar um produto
+        public async Task<IActionResult> Delete(int id)
+        {
+            //obter produto
+            var produto = await _produtosServices.GetProdutoAsync(id);
+            //retornar view
+            return View(produto);
+        }
+
+        //metodo delete, deletar um produto
+        [HttpPost, ActionName("Apagar")]
+        public async Task<IActionResult> Delete(int id, Produtos produto)
+        {
+            //deletar produto
+            await _produtosServices.DeleteAsync(produto.ProdutoId);
+            //redirecionar para a pagina inicial
+            return RedirectToAction(nameof(Index));
+        }
 
     }
 }
--- AluraGeekMVCTeste/Models/Services/ProdutosServices.cs
+++ AluraGeekMVCTeste/Models/Services/ProdutosServices.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
 
 namespace AluraGeekMVCTeste.Models.Services
 {
@@ -6,6 +8,11 @@ namespace AluraGeekMVCTeste.Models.Services
     {
         //const string
         private readonly string apiEndpoint = "/produtos/";
+        private readonly string apiEndpointBuscar = "/produtos/buscar/id/";
+        private readonly string apiEndpointCadastrar = "/produtos/cadastrar";
+        private readonly string apiEndpointEditar = "/Produtos/Editar/";
+        private readonly string apiEndpointDeletar = "/Produtos/Deletar/";
+
 
         private readonly JsonSerializerOptions _options;
 
@@ -25,7 +32,7 @@ namespace AluraGeekMVCTeste.Models.Services
             };
         }
 
-        //metodo assincrono
+        //metodo assincrono, lista todos os produtos
         public async Task<IEnumerable<Produtos>> GetProdutosAsync()
         {
             //criar cliente
@@ -52,15 +59,18 @@ namespace AluraGeekMVCTeste.Models.Services
             }
         }
 
+        //pega um produto pelo id
         public async Task<Produtos> GetProdutoAsync(int id)
         {
+            
+            
             var client = _clientFactory.CreateClient("AluraGeek");
-            using (var response = await client.GetAsync(apiEndpoint + id))
+            using (var response = await client.GetAsync(apiEndpointBuscar + id))
             {
                 if (response.IsSuccessStatusCode)
                 {
-                    using var responseStream = await response.Content.ReadAsStreamAsync();
-                    produto = await JsonSerializer.DeserializeAsync<Produtos>(responseStream, _options);
+                    using (var responseStream = await response.Content.ReadAsStreamAsync())
+                        produto = await JsonSerializer.DeserializeAsync<Produtos>(responseStream, _options);
                 }
                 else
                 {
@@ -71,6 +81,66 @@ namespace AluraGeekMVCTeste.Models.Services
             }
         }
 
+        //metodo para cadastrar um produto
+        public async Task<Produtos> CreateProdutoAsync(Produtos produto)
+        {
+            var client = _clientFactory.CreateClient("AluraGeek");
+            var produtoJson = new StringContent(JsonSerializer.Serialize(produto), Encoding.UTF8, "application/json");
+            using (var response = await client.PostAsync(apiEndpointCadastrar, produtoJson))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    using (var responseStream = await response.Content.ReadAsStreamAsync())
+                        produto = await JsonSerializer.DeserializeAsync<Produtos>(responseStream, _options);
+                    
+                }
+                else
+                {
+                    return (null);
+                }
+                return produto;
 
+            }
+        }
+
+        //metodo para modificar um produto
+        public async Task<Produtos> UpdateProdutoAsync(Produtos produto, int id)
+        {
+            var client = _clientFactory.CreateClient("AluraGeek");
+            var produtoJson = new StringContent(JsonSerializer.Serialize(produto), Encoding.UTF8, "application/json");
+            using (var response = await client.PutAsJsonAsync(apiEndpointEditar + id, produto))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    using (var responseStream = await response.Content.ReadAsStreamAsync())
+                        produto = await JsonSerializer.DeserializeAsync<Produtos>(responseStream, _options);
+                }
+                else
+                {
+                    return (null);
+                }
+                return produto;
+            }
+        }
+
+
+        //metodo para deletar um produto
+        public async Task<Produtos> DeleteAsync(int id)
+        {
+            var client = _clientFactory.CreateClient("AluraGeek");
+            using (var response = await client.DeleteAsync(apiEndpointDeletar + id))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    using (var responseStream = await response.Content.ReadAsStreamAsync())
+                        produto = await JsonSerializer.DeserializeAsync<Produtos>(responseStream, _options);
+                }
+                else
+                {
+                    return (null);
+                }
+                return produto;
+            }
+        }
     }
 }
